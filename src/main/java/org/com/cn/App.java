4198d98f64535b0824125e2a534fd60d99458764
lizhihao <lizhihao@jimeng.io>
2022-07-20 10:09:27 +0800

    1

--- src/main/java/org/com/cn/App.java
+++ src/main/java/org/com/cn/App.java
@@ -3,7 +3,6 @@ package org.com.cn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Configuration
